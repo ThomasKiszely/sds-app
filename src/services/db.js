503cd43bfd1c96454569075e2b8401c08f72b58c
ThomasKiszely <thomas@bohn-jacobsen.dk>
2026-09-22 21:13:40 +0200

    har ændret db

--- src/services/db.js
+++ src/services/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+//har ændret til mongoUri
 async function connectToMongo() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
